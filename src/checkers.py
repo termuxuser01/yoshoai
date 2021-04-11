4e2fdf2621de0f32a07ba591ac792b444d2d7681
termuxuser01 <termuxuser01@gmail.com>
2021-04-11 00:38:12 -0600

    added function checking i n checker

--- src/checkers.py
+++ src/checkers.py
@@ -1,7 +1,15 @@
 #temporary file for functions that checki input, in the mean time
 from src.resp_gen import *
+from src.functions import *
 
 def checker(input_st):
+    
+    #check for function command
+    if(checkFunc(input_st)):
+        return "done", False
+
+
+
     if(UserDismiss(input_st)):
         quit = True
         return YoshoDismiss(), quit
@@ -34,9 +42,18 @@ def lbomb(input_st):
     input_st = input_st.lower().split()
 
     result = all([(lb_comp[0] in input_st), (lb_comp[1] in input_st), (lb_comp[2] in input_st)])
-    print(result)
 
     if(result):
         return True
+    else:
+        return False
+
+def checkFunc(input_st):
+    functions = [
+            ("turn off my computer", "shutdown my computer")
+            ]
+
+    if(input_st == functions[0][0] | input_st == functions[0][1]):
+        ShutDown()
     else:
         return False
