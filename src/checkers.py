e13564607e6c97f95edb01dc88451b23c3a040f5
termuxuser01 <termuxuser01@gmail.com>
2021-04-11 00:18:03 -0600

    Updated with lbomb checker

--- src/checkers.py
+++ src/checkers.py
@@ -5,6 +5,8 @@ def checker(input_st):
     if(UserDismiss(input_st)):
         quit = True
         return YoshoDismiss(), quit
+    elif(lbomb(input_st)):
+        return Yosholbomb(), False
     else:
         quit = False
         return False, quit
@@ -17,10 +19,24 @@ def UserDismiss(input_st):
             "ciao", "bye now"
             ]
     for bye in byes:
-        if(input_st == bye):
+        if(input_st.lower() == bye):
             return True
         else:
             continue
 
     return False
 
+def lbomb(input_st):
+    lb_comp = [
+            "i", "love", "you"
+            ]
+
+    input_st = input_st.lower().split()
+
+    result = all([(lb_comp[0] in input_st), (lb_comp[1] in input_st), (lb_comp[2] in input_st)])
+    print(result)
+
+    if(result):
+        return True
+    else:
+        return False
